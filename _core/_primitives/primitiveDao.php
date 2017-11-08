ab923bcdece4dfd7163be56da7a23c1056fefd3c
Rony Nogueira <ronynogueiras.dev@gmail.com>
2017-11-08 01:56:07 -0200

    correção em carregamento de bd e identação

--- _core/_primitives/primitiveDao.php
+++ _core/_primitives/primitiveDao.php
@@ -2,7 +2,7 @@
 //table is set in variable $table
 
 
-class Dao 
+class Dao
 {
 	private $name;
 	private $database;
@@ -10,78 +10,79 @@ class Dao
 	private $order = 'ASC';
 	private $lastInsert;
 	public $App;
-	
+
 	private function __construct( $daoName , &$parentApp )
 	{
 		$this->name = $daoName;
 		$this->App = &$parentApp;
-		 
+
 		$this->loadDatabaseService();
 		$this->afterConstruct();
 	}
-	
-	public static function create( $daoName, $parentApp ) 
+
+	public static function create( $daoName, $parentApp )
 	{
 		return new static( $daoName, $parentApp );
 	}
-	
+
 	public function __destruct()
-	{	
+	{
 		$this->afterDestruct();
 	}
-	
+
 	public function afterConstruct()
 	{
-		
+
 	}
-	
+
 	public function afterDestruct()
 	{
-		
+
 	}
-	
+
 	public function setDbTable( $table )
 	{
 		$this->table = $table;
 	}
-	
+
 	public function getDbTable( $table )
 	{
 		return $this->table;
 	}
-	
+
 	public function greet()
 	{
 		echo "DAO <b>" . $this->name . "</b> Online <br/>";
 	}
-	
-    protected function loadDatabaseService()
+
+  protected function loadDatabaseService()
 	{
     	$serviceName = 'mysql';
-      	$serviceClassName = ucfirst( $serviceName );
-  
-      	$fileForInclusion = SERVICES . '' . $serviceName . '.php';
-      	if ( file_exists( $fileForInclusion ) ) {
-        	include_once( $fileForInclusion );
-        	$this->{$serviceName.'Service'} = $serviceClassName::create( $this->name . '->' . $serviceName, $this->App );
-      	}
-      	else {
-        	$this->App->exceptionsService->raise( 'serviceNotFound' );
-      	}
-    }
-	
+    	$serviceClassName = ucfirst( $serviceName );
+
+    	$fileForInclusion = SERVICES . '' . $serviceName . '.php';
+    	if ( file_exists( $fileForInclusion ) ) {
+      	include_once( $fileForInclusion );
+      	$this->{$serviceName.'Service'} = $serviceClassName::create( $this->name . '->' . $serviceName, $this->App );
+				$this->{$serviceName.'Service'}->afterConstruct();
+    	}
+    	else {
+      	$this->App->exceptionsService->raise( 'serviceNotFound' );
+    	}
+  }
+
 	public function orderUp()
 	{
 		$this->order = 'ASC';
 	}
-	
+
 	public function orderDown()
 	{
 		$this->order = 'DESC';
 	}
-	
+
 	public function find( $key = null, $value = null, $fields = null )
-	{	
+	{
 		if ( $fields == null ) {
 			$fieldstring = "*";
 		}
@@ -95,8 +96,8 @@ class Dao
 		else {
 			$sql = "SELECT " . $fieldstring . " FROM " . $this->table . " WHERE `" . $key . "` = '" . $value . "' ORDER BY id " . $this->order;
 		}
-		
-		$result = $this->database->query( $sql );
+		// necessita ajuste
+		$result = $this->mysqlService->query( $sql );
 		return $result->fetchAll();
 	}
 
@@ -105,42 +106,42 @@ class Dao
 		for ( $i = 0; $i < count( $data ); $i++ ) {
 			$data[$i] = htmlentities( $data[$i], ENT_QUOTES );
 		}
-		
+
 		$fieldstring = "`" . implode( "`,`", $fields ) . "`";
 		$datastring = "'" . implode( "','", $data ) . "'";
 		$sql = "INSERT INTO " . $this->table . " (" . $fieldstring . ") VALUES (" . $datastring . ")";
 		$result = $this->database->query( $sql );
 		$this->lastInsert =  $this->database->lastInsert;
-	
+
 		return $result;
 	}
 
-	public function lastInsert() 
+	public function lastInsert()
 	{
 		return $this->database->lastInsert;
 	}
 
 	public function remove( $key, $value )
-	{	
+	{
 		$sql = "DELETE FROM " . $this->table . " WHERE `" . $key . "`='" . $value . "'";
 		$result = $this->database->query( $sql );
 		return $result;
 	}
-	
+
 	public function update( $key, $value, $fields, $sdata )
 	{
-		$setstring = "";	
-		
+		$setstring = "";
+
 		for ( $i = 0; $i < count( $fields ); $i++ ){
 			$setstring = $setstring . "`" . $fields[$i] . "`='" . htmlentities( $sdata[$i], ENT_QUOTES ) . "'";
 			if ( $i < count( $fields ) - 1 ) {
 				$setstring = $setstring . ",";
-			} 
+			}
 		}
-		
+
 		$sql =  "UPDATE " . $this->table . " SET " . $setstring . " WHERE `" . $key . "` = '" . $value . "'";
-		$result = $this->database->query( $sql );		
+		$result = $this->database->query( $sql );
 		return $result;
 	}
-	
+
 }
